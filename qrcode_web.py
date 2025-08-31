51672e6913ddc313023b697ae3c6ea21ddf1a5a5
redhat4u <redhat4u@gmail.com>
2025-08-31 14:46:02 +0900

    Update qrcode_web.py

--- qrcode_web.py
+++ qrcode_web.py
@@ -201,29 +201,19 @@ with col1:
 
     st.subheader("🔧 파일 설정")
     
-    col_filename1, col_filename2 = st.columns([3,1])
-    with col_filename1:
-        filename = st.text_input(
-            "다운로드 파일명 입력 (확장자는 제외, 파일명만 입력)",
-            placeholder="이 곳에 파일명을 입력해 주세요 (비어있으면 자동 생성됨)",
-            key="filename_input"
-        )
-    with col_filename2:
-        # 버튼 높이 조절
-        st.markdown(
-            """
-            <style>
-            div[data-testid="stVerticalBlock"] div.stButton button {
-                height: 3rem;
-                margin-top: 0.25rem;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+    col_label1, col_label2 = st.columns([3,1])
+    with col_label1:
+        st.write("다운로드 파일명 입력 (확장자는 제외)")
+    with col_label2:
         if st.button("❌ 삭제", use_container_width=True):
             st.session_state.filename_input = ""
             st.success("파일명이 삭제되었습니다.")
+
+    filename = st.text_input(
+        "",
+        placeholder="이 곳에 파일명을 입력해 주세요 (비어있으면 자동 생성됨)",
+        key="filename_input"
+    )
 
     if "last_filename" not in st.session_state:
         st.session_state.last_filename = ""  # 처음엔 빈 문자열로 시작
