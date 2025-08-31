06199717953ad0b3ef94a176b7a24291073c0d0a
redhat4u <redhat4u@gmail.com>
2025-08-31 16:17:18 +0900

    Update qrcode_web.py

--- qrcode_web.py
+++ qrcode_web.py
@@ -424,8 +424,9 @@ with st.sidebar:
 # 하단 정보
 st.markdown("---")
 st.markdown(
-    '<p style="text-align: center; color: darkblue; font-size: 16px;">© 2025 QR 코드 생성기  |  Streamlit으로 제작  |  제작: 류종훈(redhat4u@gmail.com)</p>',
+    '<p style="text-align: center; color: orange; font-size: 16px;">© 2025 QR 코드 생성기  |  Streamlit으로 제작  |  제작: 류종훈(redhat4u@gmail.com)</p>',
     unsafe_allow_html=True
 )
+
 
 
