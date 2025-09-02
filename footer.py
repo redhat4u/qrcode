8d6b99d6c0b19ca8898002049a2489abdb007486
redhat4u <redhat4u@gmail.com>
2025-09-02 22:09:08 +0900

    Update footer.py

--- footer.py
+++ footer.py
@@ -2,13 +2,13 @@
 # footer.py
 
 import streamlit as st
-from messages import * # <-- 추가
+from messages import FOOTER_MESSAGE
 
 def build_footer():
     """앱 하단 정보를 빌드합니다."""
     st.markdown("---")
     st.markdown(
-        f'<p style="text-align: center; color: hotpink; font-size: 15px;">{FOOTER_TEXT}</p>', # <-- 수정
+        f'<p style="text-align: center; color: hotpink; font-size: 15px;">{FOOTER_MESSAGE}</p>',
         unsafe_allow_html=True
     )
     
