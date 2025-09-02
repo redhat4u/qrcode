f3d5e0983e9bb2019d3069c4ba210e645ad9565a
redhat4u <redhat4u@gmail.com>
2025-09-02 22:53:24 +0900

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,3 +1,4 @@
+# 이 파일은 사이드바 UI를 구성하는 함수를 정의합니다.
 # sidebar.py
 
 import streamlit as st
@@ -5,27 +6,24 @@ from messages import get_message
 
 def build_sidebar_ui():
     """사이드바를 빌드합니다."""
-    
-    # 언어 선택 드롭다운 메뉴
-    st.sidebar.header(get_message('SIDEBAR_HEADER_LANG'))
-    lang_options = [get_message('LANG_KO'), get_message('LANG_EN')]
-    
-    # 현재 선택된 언어에 따라 index를 설정
-    if st.session_state.current_lang == 'ko':
-        default_index = 0
-    else:
-        default_index = 1
-        
-    selected_lang = st.sidebar.selectbox(
-        get_message('SELECTBOX_LANG_LABEL'),
-        options=lang_options,
-        index=default_index,
-    )
 
-    if selected_lang == get_message('LANG_KO'):
-        st.session_state.current_lang = 'ko'
-    else:
-        st.session_state.current_lang = 'en'
+    # 언어 선택 관련 코드 삭제
+    # st.sidebar.header(get_message('SIDEBAR_HEADER_LANG'))
+    # lang_options = [get_message('LANG_KO'), get_message('LANG_EN')]
+    # if st.session_state.current_lang == 'ko':
+    #     default_index = 0
+    # else:
+    #     default_index = 1
+    # selected_lang = st.sidebar.selectbox(
+    #     get_message('SELECTBOX_LANG_LABEL'),
+    #     options=lang_options,
+    #     index=default_index,
+    #     key='lang_select_box_sidebar'
+    # )
+    # if selected_lang == get_message('LANG_KO'):
+    #     st.session_state.current_lang = 'ko'
+    # else:
+    #     st.session_state.current_lang = 'en'
     
     st.markdown("---")
 
