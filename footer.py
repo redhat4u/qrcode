cf9d45ae65ad7b7cf6a0ecd5745d5d551b89ee3d
redhat4u <redhat4u@gmail.com>
2025-09-02 22:37:17 +0900

    Update footer.py

--- footer.py
+++ footer.py
@@ -1,14 +1,14 @@
-# 이 파일은 화면 하단의 제작 정보를 나타내는 함수를 정의합니다.
 # footer.py
 
 import streamlit as st
-from messages import FOOTER_MESSAGE
+from messages import get_message # FOOTER_MESSAGE 대신 get_message 함수만 가져옵니다.
 
 def build_footer():
     """앱 하단 정보를 빌드합니다."""
     st.markdown("---")
+    footer_message_text = get_message('FOOTER_MESSAGE')
     st.markdown(
-        f'<p style="text-align: center; color: hotpink; font-size: 15px;">{FOOTER_MESSAGE}</p>',
+        f'<p style=\"text-align: center; color: hotpink; font-size: 15px;\"> {footer_message_text} </p>',
         unsafe_allow_html=True
     )
     
