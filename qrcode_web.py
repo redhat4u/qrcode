bfc0b4f7f021b31fc6fc62cad5e79cb14d4efb9f
redhat4u <redhat4u@gmail.com>
2025-09-02 22:46:50 +0900

    Update qrcode_web.py

--- qrcode_web.py
+++ qrcode_web.py
@@ -15,66 +15,44 @@ from state_manager import (
     clear_filename_callback,
     on_qr_setting_change,
     set_download_initiated,
-    set_lang_ko, # 추가
-    set_lang_en, # 추가
 )
-
 from functions import (
     sanitize_filename,
     is_valid_color,
     generate_qr_code_png,
     generate_qr_code_svg,
 )
-
 from ui_input_and_settings import build_input_and_settings_ui
 from ui_preview_and_download import build_preview_and_download_ui
 from sidebar import build_sidebar_ui
 from footer import build_footer
-from messages import get_message # get_message 함수만 가져오도록 수정
-
-# 페이지 설정
-st.set_page_config(
-    page_title=get_message('APP_TITLE'), # 수정
-    page_icon="🔲",
-    layout="wide",
-)
+from messages import get_message # <-- 이 줄을 추가합니다.
 
 # 세션 상태 초기화
 initialize_session_state()
 
-# 메인 앱 헤더
-header_col1, header_col2, header_col3 = st.columns([10, 1, 1])
-with header_col1:
-    st.title(get_message('APP_MAIN_HEADER')) # 수정
-with header_col2:
-    st.button("🇰🇷", key="lang_ko", on_click=set_lang_ko)
-with header_col3:
-    st.button("🇺🇸", key="lang_en", on_click=set_lang_en)
+# 페이지 설정
+st.set_page_config(
+    page_title=get_message('APP_TITLE'), # <-- 이 부분을 수정합니다.
+    page_icon="🔲",
+    layout="wide",
+)
 
+# 메인 앱 헤더
+st.title("🔲 " + get_message('APP_TITLE')) # <-- 이 부분을 수정합니다.
 st.markdown("---")
 
 # 레이아웃 설정 (2개 컬럼)
-col1, col2 = st.columns([1.2, 1])
+col_input, col_preview = st.columns([1.5, 1])
 
-# 각 섹션의 UI를 별도의 함수로 분리하여 호출
-with col1:
+with col_input:
     build_input_and_settings_ui()
-with col2:
+with col_preview:
     build_preview_and_download_ui()
 
-# 전체 초기화 버튼
-st.markdown("---")
-st.button(
-    label=get_message('APP_RESET_BUTTON_LABEL'), # 수정
-    use_container_width=True,
-    type="secondary",
-    on_click=reset_all_settings,
-    help=get_message('APP_RESET_BUTTON_HELP'), # 수정
-)
-
-# 사이드바를 별도 파일에서 만든 함수로 호출
+# 사이드바 UI 빌드
 with st.sidebar:
     build_sidebar_ui()
 
-# 하단 정보
+# 푸터 빌드
 build_footer()
