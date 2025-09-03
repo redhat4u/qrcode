17af288e21fe0d3c791e906558a711c2237ce333
redhat4u <redhat4u@gmail.com>
2025-09-04 06:46:01 +0900

    Update qrcode_web.py

--- qrcode_web.py
+++ qrcode_web.py
@@ -203,7 +203,7 @@ def draw_custom_shape_image(qr_object, box_size, border, fill_color, back_color,
         elif shape == lang_messages['pattern_shape_cross']:
             x_center = (new_x + new_x_end) / 2
             y_center = (new_y + new_y_end) / 2
-            cross_width = effective_size_after_gap * 0.3
+            cross_width = effective_size_after_gap * 0.5
             draw.rectangle([new_x, y_center - cross_width/2, new_x_end, y_center + cross_width/2], fill=fill,)
             draw.rectangle([x_center - cross_width/2, new_y, x_center + cross_width/2, new_y_end], fill=fill,)
 
