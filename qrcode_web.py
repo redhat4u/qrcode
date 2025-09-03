60bd73f8fb2b81483d1a92e151191d4f6642915e
redhat4u <redhat4u@gmail.com>
2025-09-03 12:55:03 +0900

    Update qrcode_web.py

--- qrcode_web.py
+++ qrcode_web.py
@@ -595,7 +595,7 @@ with col2:
         st.subheader(lang_messages['preview_subheader'])
         col_left, col_center, col_right = st.columns([1, 2, 1])
         with col_center:
-            st.image(preview_image_display, caption=lang_messages['preview_subheader'], width=374)
+            st.image(preview_image_display, caption=lang_messages['preview_subheader'], width=376)
 
         if preview_qr_object:
             st.subheader(lang_messages['qr_info_header'])
